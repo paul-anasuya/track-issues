ac3ad4face942d95b1a4e8185775b0cad92873a5
Anasuya Paul <paul.anasuya@tcs.com>
2018-08-07 11:50:57 +0530

    Added newRelic monitoring script

---
+++ monitor.js
@@ -0,0 +1,26 @@
+var request = require('request');
+
+const URL = 'https://api.newrelic.com/v2/applications.json';
+
+var options = {
+   method: 'GET',
+   url: URL,
+   headers: {
+      'Accept': 'application/json',
+      'X-Api-Key': process.env.NEWRELIC_REST_API_KEY
+   }
+};
+
+request(options, function (error, response, body) {
+   if (error) throw new Error(error);
+   console.log(
+      'Response: ' + response.statusCode + ' ' + response.statusMessage
+   );
+   //console.log(body);
+   var jsonContent = JSON.parse(body);
+   for (var i=0; i<jsonContent.applications.length; i++){
+      if(jsonContent.applications[i].name == process.env.npm_config_app_name) {
+                console.log("app exist with name: "+jsonContent.applications[i].name);      
+        }
+    }
+});
